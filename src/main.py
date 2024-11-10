4d6919b17036b592d66764eb0e2d215b29259892
Muaaz Butt <muaazbutt585@gmail.com>
2024-11-10 15:55:22 +0500

    Split the permuted 64 bit into two 32 bits keys

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from master_key_generator import generate_master_key
 from initial_permutation import initial_permutation
+from split_key import split_key_into_32_bits
 
 def main():
     name = "MUAAZBUTT"
@@ -8,6 +9,10 @@ def main():
     
     permutated_key = initial_permutation(master_key)
     print(f"Permuted Master Key: {permutated_key}")
+    
+    left, right = split_key_into_32_bits(permutated_key)
+    print (f"left half: {left}")
+    print (f"right half: {right}")
     
 if __name__ == "__main__":
     main()
